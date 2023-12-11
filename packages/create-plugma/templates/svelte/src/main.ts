45f961b0472b0c40395b495d283ce1d046772749
Gavin McFarland <5551+gavinmcfarland@users.noreply.github.com>
2023-12-11 15:32:34 +0000

    Work in progress

--- packages/create-plugma/templates/svelte/src/main.ts
+++ packages/create-plugma/templates/svelte/src/main.ts
@@ -1,4 +1,8 @@
-// interceptHtmlString
-export default function () {
-	figma.closePlugin("Goodbye");
-}
+import { __html__ } from "plugma/frameworks/common/main/interceptHtmlString";
+import { saveFigmaStyles } from "plugma/frameworks/common/main/saveFigmaStyles";
+
+// Your app code below
+
+figma.showUI(__html__, { width: 300, height: 500, themeColors: true });
+
+saveFigmaStyles();
