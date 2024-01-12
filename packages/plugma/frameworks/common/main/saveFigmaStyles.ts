9aa319074c1c2daf54920bfe1c96a02ed33f0fbe
Gavin McFarland <5551+gavinmcfarland@users.noreply.github.com>
2024-01-12 17:58:52 +0000

    Work in progress

--- packages/plugma/frameworks/common/main/saveFigmaStyles.ts
+++ packages/plugma/frameworks/common/main/saveFigmaStyles.ts
@@ -1,22 +1,21 @@
 function saveFigmaStyles() {
-	if (
-		process.env.NODE_ENV === "development" ||
-		process.env.NODE_ENV === "server"
-	) {
-		figma.ui.onmessage = async (msg) => {
-			if (msg.event === "save-figma-stylesheet") {
-				figma.clientStorage.setAsync("figma-stylesheet", msg.styles);
-			}
-			if (msg.event === "get-figma-stylesheet") {
-				let styles =
-					await figma.clientStorage.getAsync("figma-stylesheet");
-				figma.ui.postMessage({
-					event: "pass-figma-stylesheet",
-					styles,
-				});
-			}
-		};
-	}
+  if (
+    process.env.NODE_ENV === "development" ||
+    process.env.NODE_ENV === "server"
+  ) {
+    figma.ui.on("message", async (msg) => {
+      if (msg.event === "save-figma-stylesheet") {
+        figma.clientStorage.setAsync("figma-stylesheet", msg.styles);
+      }
+      if (msg.event === "get-figma-stylesheet") {
+        let styles = await figma.clientStorage.getAsync("figma-stylesheet");
+        figma.ui.postMessage({
+          event: "pass-figma-stylesheet",
+          styles,
+        });
+      }
+    });
+  }
 }
 
 export { saveFigmaStyles };
