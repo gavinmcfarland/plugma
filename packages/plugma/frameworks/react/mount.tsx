a090af0ba9edb5e83b92d5320dfec6b34887b64d
Gavin McFarland <5551+gavinmcfarland@users.noreply.github.com>
2024-06-07 15:37:36 +0100

    Updated package

--- packages/plugma/frameworks/react/mount.tsx
+++ packages/plugma/frameworks/react/mount.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 export function mount(App) {
-	ReactDOM.createRoot(document.getElementById('root')!).render(
+	ReactDOM.createRoot(document.getElementById('app')!).render(
 		<React.StrictMode>
 			<App />
 		</React.StrictMode>,
