481e2789b593eaa2bda746933f1f032ba23cd41b
Gavin McFarland <5551+gavinmcfarland@users.noreply.github.com>
2024-10-03 23:34:30 +0100

    refactored run script

--- packages/plugma/scripts/run-script.js
+++ packages/plugma/scripts/run-script.js
@@ -1,49 +1,47 @@
-import { createServer } from 'vite';
+import { createServer, build } from 'vite';
 import esbuild from 'esbuild';
 import fse from 'fs-extra';
-import { exec } from 'child_process';
-import { dirname, resolve, parse, join } from 'path';
 import fs from 'fs';
-import os, { type } from 'os';
+import path, { dirname, resolve, join } from 'path';
+import { exec } from 'child_process';
+import os from 'os';
 import chalk from 'chalk';
-import { build } from 'vite'
-import { replace } from 'esbuild-plugin-replace';
 import { fileURLToPath } from 'url';
+import lodashTemplate from 'lodash.template';
+import _ from 'lodash';
+
 import envfilePlugin from '../lib/esbuild-plugins/esbuild-plugin-envfile.js';
 import htmlTransform from '../lib/vite-plugins/vite-plugin-html-transform.js';
 import replaceMainInput from '../lib/vite-plugins/vite-plugin-replace-main-input.js';
 import deepIndex from '../lib/vite-plugins/vite-plugin-deep-index.js';
 import viteCopyDirectoryPlugin from '../lib/vite-plugins/vite-plugin-copy-dir.js';
-import { viteSingleFile } from "vite-plugin-singlefile";
-import _ from 'lodash';
-import { Log } from '../lib/logger.js'
-import lodashTemplate from 'lodash.template'
+import { viteSingleFile } from 'vite-plugin-singlefile';
 import globalPolyfill from '../lib/esbuild-plugins/esbuild-plugin-global-polyfill.js';
+import { Log } from '../lib/logger.js';
 
 const CURR_DIR = process.cwd();
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const __filename = fileURLToPath(import.meta.url);
 
 export default async function cli(command, options) {
-	const log = new Log({
-		debug: options.debug
-	})
+	const log = new Log({ debug: options.debug });
 
-	// Allow CLI to set NODE_ENV
 	process.env.NODE_ENV = options.mode || 'development';
-	options.port = options.port || getRandomNumber()
+	options.port = options.port || getRandomNumber();
 
-	const pkg = await getJsonFile(resolve(`${__dirname}/../package.json`));
+	const pkg = await readJson(resolve(`${__dirname}/../package.json`));
 	const data = await getFiles();
+
 	const viteConfigs = createViteConfigs(options, data);
+	const esbuildConfigs = createEsbuildConfigs(options, data);
 
 	switch (command) {
 		case 'build':
-			await runBuildTask(command, options, data, viteConfigs.build, pkg, log)
+			await runBuildTask(options, data, viteConfigs.build, pkg, log, esbuildConfigs.build);
 			break;
 
 		case 'dev':
-			await runDevTask(command, options, data, viteConfigs.dev, pkg, log)
+			await runDevTask(options, data, viteConfigs.dev, pkg, log, esbuildConfigs);
 			break;
 
 		default:
@@ -52,108 +50,75 @@ export default async function cli(command, options) {
 	}
 }
 
-async function runBuildTask(command, options, data, buildViteConfig, pkg, log) {
-	if (command === "build") {
+async function runBuildTask(options, data, buildViteConfig, pkg, log, esbuildBuildConfig) {
+	log.text(`${chalk.blue.bold('Plugma')} ${chalk.grey("v" + pkg.version)}`);
 
-		log.text(`${chalk.blue.bold('Plugma')} ${chalk.grey("v" + pkg.version)}`)
+	await writeIndexFile();
+	// Build manifest
+	await writeManifestFile(data, () => log.format({ indent: 1 }).text(`manifest.json file created!`));
+	// Build main
+	await handleEsbuild(esbuildBuildConfig, options, log);
+	// Build UI
+	await handleViteBuild(buildViteConfig, options.watch, log);
+}
 
-		await writeIndexFile()
+async function runDevTask(options, data, devViteConfig, pkg, log, esbuildConfigs) {
+	log.text(`${chalk.blue.bold('Plugma')} ${chalk.grey("v" + pkg.version)}`);
 
-		// ----- build manifest.json
-		await writeManifestFile(data, () => {
-			log.format({ indent: 1 }).text(`manifest.json file created!`)
-		})
+	await writeIndexFile();
+	// Build manifest
+	await writeManifestFile(data, () => log.format({ indent: 1 }).text(`manifest.json file created!`));
+	// Build main
+	await handleEsbuild(esbuildConfigs.dev, options, log);
 
-		// ----- build main.js
-		await bundleMainWithEsbuild(data, options.watch, () => {
-			log.format({ indent: 1 }).text(`main.js file created!`)
-		}, 'production', options, command)
+	// Build ui
+	buildDevUiFile(options, log)
+	log.format({ indent: 1 }).text(`ui.html file created!`);
 
-		log.text(`\nWatching for changes...`)
+	if (options.websockets) {
+		log.format({ indent: 1 }).text(`Preview: ${chalk.cyan(`http://localhost:${options.port}/`)}`);
+	}
 
-		// ----- build ui.html (no server needed)
+	log.text(`\nWatching for changes...`);
 
+	await startViteServer(devViteConfig, options.websockets);
+
+}
+
+async function handleEsbuild(esbuildConfig, options, log) {
+	try {
 		if (options.watch) {
-			await build(_.merge({}, buildViteConfig, {
-				build: {
-					watch: {}
-				}
-			}));
+			const ctx = await esbuild.context(esbuildConfig);
+			await ctx.watch();
+		} else {
+			await esbuild.build(esbuildConfig);
+			log.format({ indent: 1 }).text(`main.js file created!`);
 		}
-		else {
-			await build(buildViteConfig)
-		}
+	} catch (err) {
+		console.error('Error bundling file with esbuild:', err);
 	}
 }
 
-async function runDevTask(command, options, data, devViteConfig, pkg, log) {
-	if (command === "dev") {
+async function handleViteBuild(viteConfig, watch, log) {
+	if (watch) {
+		await build(_.merge({}, viteConfig, { build: { watch: {} } }));
+	} else {
+		await build(viteConfig);
+	}
+	log.format({ indent: 1 }).text(`ui.html file created!`);
+}
 
-		log.text(`${chalk.blue.bold('Plugma')} ${chalk.grey("v" + pkg.version)}`);
+async function startViteServer(viteConfig, websocketsEnabled) {
+	try {
+		const server = await createServer(viteConfig);
+		await server.listen();
 
-		await writeIndexFile()
-
-		// ----- build ui.html
-		// We don't need to bundle the UI because when developing it needs to point to the dev server. So instead we create a placeholder ui file that points to a server
-
-		let devHtmlString = fs.readFileSync(resolve(`${__dirname}/../apps/PluginWindow.html`), 'utf8');
-
-		let runtimeData = `<script>
-		// Global variables defined on the window object
-		window.runtimeData = {
-			port: ${options.port},
-			debug: ${options.debug},
-			websockets: ${options.websockets}
-		};
-	</script>`
-
-		devHtmlString = devHtmlString.replace(/^/, runtimeData)
-
-		createFileWithDirectory(`${CURR_DIR}/dist`, 'ui.html', devHtmlString)
-
-		log.format({ indent: 1 }).text(`ui.html file created!`)
-
-		// ----- build manifest.json
-		await writeManifestFile(data, () => {
-			log.format({ indent: 1 }).text(`manifest.json file created!`)
-		})
-
-		// ----- build main.js
-		await bundleMainWithEsbuild(data, true, () => {
-			log.format({ indent: 1 }).text(`main.js file created!`)
-		}, 'development', options, command)
-
-		if (options.websockets) {
-			log.format({ indent: 1 }).text(`Preview: ${chalk.cyan('http://localhost:')}${chalk.bold.cyan(options.port)}${chalk.cyan('/')}`)
+		if (websocketsEnabled) {
+			exec('node node_modules/plugma/lib/start-web-sockets-server.cjs');
 		}
-
-		log.text(`\nWatching for changes...`)
-
-		// ----- run vite app server
-		try {
-
-			const server = await createServer(devViteConfig);
-
-			await server.listen(); // Start the Vite server
-
-
-			if (options.websockets) {
-				// Run a web socket server so postMessage works between browser and Figma. And so Figma theme works in browser
-				const childProcess = exec('node node_modules/plugma/lib/start-web-sockets-server.cjs');
-				// childProcess.stderr.on('data', (data) => {
-				// 	console.error(`Script error: ${data}`);
-				// });
-			}
-
-			// return server
-
-
-		} catch (err) {
-			console.error('Error starting Vite server:', err);
-			process.exit(1);
-		}
-
-
+	} catch (err) {
+		console.error('Error starting Vite server:', err);
+		process.exit(1);
 	}
 }
 
@@ -166,222 +131,157 @@ function createViteConfigs(options, data) {
 		}),
 	];
 
-	const devConfig = {
-		mode: options.mode,
-		define: {
-			'process.env.NODE_ENV': JSON.stringify(options.mode),
+	return {
+		dev: {
+			mode: options.mode,
+			define: { 'process.env.NODE_ENV': JSON.stringify(options.mode) },
+			plugins: [
+				replaceMainInput({ pluginName: data.figmaManifest.name, input: data.figmaManifest.ui }),
+				htmlTransform(options),
+				deepIndex(),
+				...commonPlugins,
+			],
+			server: { port: options.port },
 		},
-		plugins: [
-			replaceMainInput({
-				pluginName: data.figmaManifest.name,
-				input: data.figmaManifest.ui,
-			}),
-			htmlTransform(options),
-			deepIndex(),
-			...commonPlugins,
-		],
-		server: {
-			port: options.port,
-			// logLevel: 'silent',
-		},
-	};
-
-	const buildConfig = {
 		build: {
-			emptyOutDir: false,
-			rollupOptions: {
-				input: 'node_modules/plugma/tmp/index.html',
+			build: {
+				emptyOutDir: false,
+				rollupOptions: { input: 'node_modules/plugma/tmp/index.html' },
 			},
+			plugins: commonPlugins,
 		},
-		plugins: commonPlugins,
 	};
-
-	return { dev: devConfig, build: buildConfig };
 }
 
+function createEsbuildConfigs(options, data) {
+	const tempFilePath = writeTempFile(`temp_${Date.now()}.js`, data);
 
-async function getJsonFile(filePath) {
+	const commonConfig = {
+		entryPoints: [tempFilePath],
+		outfile: 'dist/main.js',
+		format: 'esm',
+		bundle: true,
+		target: 'es2016',
+		plugins: [
+			globalPolyfill(),
+			envfilePlugin({
+				envPath: '.env',
+				envTestPath: '.env.test',
+				envDevelopmentPath: '.env.development',
+			}),
+		],
+	};
 
-	return new Promise((resolve, reject) => {
-		if (fs.existsSync(filePath)) {
-			fs.readFile(filePath, 'utf8', function (err, data) {
-				if (err) {
-					reject(err);
-				}
-				// console.log(data)
-				resolve(JSON.parse(data));
+	return {
+		dev: {
+			...commonConfig,
+			inject: [resolve(`${__dirname}/../lib/global-shim.js`)],
+			define: {
+				'process.env.NODE_ENV': JSON.stringify(options.mode),
+				process: JSON.stringify({}),
+			},
+			plugins: [...commonConfig.plugins, notifyOnRebuild()],
+		},
+		build: commonConfig,
+	};
+}
+
+function notifyOnRebuild() {
+	return {
+		name: 'rebuild-notify',
+		setup(build) {
+			build.onEnd(() => {
+				console.log(`${chalk.grey(formatTime())} ${chalk.cyan.bold('[esbuild]')} ${chalk.green('rebuilt')} ${chalk.grey('/dist/main.js')}`);
 			});
-		}
-		else {
-			resolve(false)
-		}
-
-	});
+		},
+	};
 }
 
 function formatTime() {
-	var currentDate = new Date();
-	var hours = currentDate.getHours();
-	var minutes = currentDate.getMinutes();
-	var seconds = currentDate.getSeconds();
-	var meridiem = hours >= 12 ? 'PM' : 'AM';
-
-	// Convert 24-hour format to 12-hour format
-	hours = hours % 12;
-	hours = hours ? hours : 12; // Handle midnight (12 AM)
-
-	// Add leading zeros to minutes and seconds if needed
-	minutes = minutes < 10 ? '0' + minutes : minutes;
-	seconds = seconds < 10 ? '0' + seconds : seconds;
-
-	// Concatenate the formatted time
-	var formattedTime = hours + ':' + minutes + ':' + seconds + ' ' + meridiem;
-
-	return formattedTime;
+	const currentDate = new Date();
+	let hours = currentDate.getHours();
+	const minutes = String(currentDate.getMinutes()).padStart(2, '0');
+	const seconds = String(currentDate.getSeconds()).padStart(2, '0');
+	const meridiem = hours >= 12 ? 'PM' : 'AM';
+	hours = hours % 12 || 12;
+	return `${hours}:${minutes}:${seconds} ${meridiem}`;
 }
 
-async function bundleMainWithEsbuild(data, shouldWatch, callback, NODE_ENV, options, command) {
-
-
-	if (callback && typeof (callback) === "function") {
-		callback();
+async function readJson(filePath) {
+	if (fs.existsSync(filePath)) {
+		const data = await fs.promises.readFile(filePath, 'utf8');
+		return JSON.parse(data);
 	}
+	return false;
+}
 
-	try {
-
-		function writeTempFile(fileName) {
-			const tempFilePath = join(os.tmpdir(), fileName);
-			const modifiedContent = `import main from "${CURR_DIR}/${data.figmaManifest.main}";
-			main();`
-			// if (NODE_ENV === "development") {
-			fs.writeFileSync(tempFilePath, modifiedContent);
-			// }
-			return tempFilePath
-		}
-
-		const fileName = `temp_${Date.now()}.js`
-		let tempFilePath = writeTempFile(fileName)
-
-		if (command === "dev" || (command === "build" && options.watch)) {
-
-
-			let ctx = await esbuild.context({
-				entryPoints: [tempFilePath],
-				outfile: `dist/main.js`,
-				format: 'esm',
-				bundle: true,
-				target: 'es2016',
-				inject: [resolve(`${__dirname}/../lib/global-shim.js`)],
-				define: {
-					'process.env.NODE_ENV': JSON.stringify(options.mode),
-					'process': JSON.stringify({}),
-				},
-
-				plugins: [
-					globalPolyfill(),
-					envfilePlugin({
-						envPath: '.env',
-						envTestPath: '.env.test',
-						envDevelopmentPath: '.env.development',
-					}),
-					{
-						name: 'rebuild-notify',
-						setup(build) {
-							// build.onLoad({ filter: /\.txt$/ }, async (args) => {
-							// 	let text = await fs.promises.readFile(args.path, 'utf8')
-							// 	return {
-							// 		contents: JSON.stringify(text.split(/\s+/)),
-							// 		loader: 'json',
-							// 	}
-							// })
-							// build.onStart(() => {
-							// 	tempFilePath = writeTempFile(fileName)
-							// 	console.log('build started')
-							// })
-							// build.onStart(() => {
-							// 	console.log('Rebuilding...');
-							// });
-							build.onEnd(async result => {
-								console.log(`${chalk.grey(formatTime())} ${chalk.cyan.bold('[esbuild]')} ${chalk.green('rebuilt')} ${chalk.grey('/dist/main.js')}`)
-								// console.log(`main.ts built with ${result.errors.length} errors`);
-								// HERE: somehow restart the server from here, e.g., by sending a signal that you trap and react to inside the server.
-								// await fs.unlink(tempFilePath, (err => {
-								// 	if (err) console.log(err);
-								// }));
-							})
-						}
-						,
-					},
-					replace({
-						'__buildVersion': '"1.0.0"',
-					})
-				],
-			});
-			await ctx.watch();
-
-			// const envFiles = ['.env', '.env.test', '.env.development'].filter(file => fs.existsSync(file));
-			// envFiles.forEach(file => {
-			// 	fs.watch(file, async (eventType) => {
-			// 		if (eventType === 'change') {
-			// 			console.log(`${file} has changed. Rebuilding...`);
-
-			// 			// Manually reload environment variables
-			// 			await ctx.rebuild().catch((err) => console.error(err));
-			// 		}
-			// 	});
-			// });
-
-
-
-		} else {
-			// Fix me, needs to output js file
-			// Bundle your .mjs file using esbuild
-			await esbuild.build({
-				entryPoints: [tempFilePath],
-				outfile: `dist/main.js`,
-				format: 'esm',
-				bundle: true,
-				target: 'es2016',
-				define: {
-					'process.env.NODE_ENV': JSON.stringify(options.mode),
-				},
-				plugins: [
-					globalPolyfill(),
-					envfilePlugin({
-						envPath: '.env',
-						envTestPath: '.env.test',
-						envDevelopmentPath: '.env.development',
-					}),
-				]
-			});
-		}
-
-
-
-		// console.log('Main bundled successfully with esbuild!');
-	} catch (err) {
-		console.error('Error bundling file with esbuild:', err);
-	}
+function writeTempFile(fileName, data) {
+	const tempFilePath = join(os.tmpdir(), fileName);
+	const modifiedContent = `import main from "${CURR_DIR}/${data.figmaManifest.main}"; main();`;
+	fs.writeFileSync(tempFilePath, modifiedContent);
+	return tempFilePath;
 }
 
 async function getFiles() {
+	const rootManifest = await readJson('./manifest.json');
+	const pkg = await readJson(resolve('./package.json'));
+	const figmaManifest = rootManifest || pkg.plugma?.manifest;
 
-	let rootManifest = await getJsonFile(`./manifest.json`)
-	let pkg = await getJsonFile(resolve(`./package.json`));
-	let figmaManifest = rootManifest || pkg.plugma?.manifest;
-
-
-	// Check if name missing from manifest in package or package.json
 	if (!pkg.plugma?.manifest?.name && !rootManifest?.name) {
-		console.warn(`Plugma: Using package name as plugin name will be depreciated. Please specify the name in the manifest.
-
-Example: \`{ name: "My Plugin" }\``)
+		console.warn(`Plugma: Please specify the name in the manifest. Example: \`{ name: "My Plugin" }\``);
 	}
 
-	return {
-		figmaManifest,
-		pkg
-	}
+	return { figmaManifest, pkg };
+}
+
+async function writeManifestFile(data, callback) {
+	const pluginName = data.figmaManifest.name || data.pkg.name;
+	const newManifest = {
+		...data.figmaManifest,
+		name: pluginName,
+		api: '1.0.0',
+		main: 'main.js',
+		ui: 'ui.html',
+	};
+	await fse.outputFile('./dist/manifest.json', JSON.stringify(newManifest, null, 2));
+	if (callback) callback();
+}
+
+async function writeIndexFile() {
+	const indexTemplatePath = `${__dirname}/../templates/index.html`;
+	const newIndexPath = `${__dirname}/../tmp/index.html`;
+	const contents = await fs.promises.readFile(indexTemplatePath, 'utf8');
+	const pkg = await readJson(resolve(`${CURR_DIR}/package.json`));
+	const manifest = await readJson(resolve('./manifest.json')) || pkg.plugma.manifest;
+
+	const input = resolve('/', manifest?.ui || '/src/ui.ts');
+	const comptempl = lodashTemplate(contents);
+	const finalContent = comptempl({ name: 'figma', input });
+
+	await fse.outputFile(newIndexPath, finalContent);
+}
+
+function getRandomNumber() {
+	return Math.floor(Math.random() * (6999 - 3000 + 1)) + 3000;
+}
+
+function buildDevUiFile(options, log) {
+	let devHtmlString = fs.readFileSync(resolve(`${__dirname}/../apps/PluginWindow.html`), 'utf8');
+
+	let runtimeData = `<script>
+		// Global variables defined on the window object
+		window.runtimeData = {
+			port: ${options.port},
+			debug: ${options.debug},
+			websockets: ${options.websockets}
+		};
+	</script>`
+
+	devHtmlString = devHtmlString.replace(/^/, runtimeData)
+
+	createFileWithDirectory(`${CURR_DIR}/dist`, 'ui.html', devHtmlString)
+
+	log.format({ indent: 1 }).text(`ui.html file created!`)
 }
 
 function createFileWithDirectory(filePath, fileName, fileContent, callback) {
@@ -411,55 +311,4 @@ function createFileWithDirectory(filePath, fileName, fileContent, callback) {
 			});
 		}
 	});
-}
-
-async function writeManifestFile(data, callback) {
-	if (callback && typeof (callback) === "function") {
-		callback();
-	}
-
-	let pluginName = data.figmaManifest.name ? data.figmaManifest.name : data.pkg.name
-
-	let newManifest = {
-		...data.figmaManifest, ...{
-			"name": `${pluginName}`,
-			"api": "1.0.0",
-			"main": "main.js",
-			"ui": "ui.html",
-		}
-	}
-
-	return await createFileWithDirectory("./dist", "manifest.json", JSON.stringify(newManifest, null, 2))
-}
-
-async function writeIndexFile() {
-	// let newIndexPath = `${CURR_DIR}/${projectName}/node_modules/plugma/index.html`;
-
-	let indexTemplatePath = `${__dirname}/../templates/index.html`
-	// Rewrite using os.tmpdir(), but to work, rewriteIndexFile needs to output file location to vite config
-	let newIndexPath = `${__dirname}/../tmp/index.html`
-
-	// Need to use process.env.INIT_CWD because otherwise package is referenced from the module and not the users project <- not sure this is true
-	let pkgPath = resolve(`${CURR_DIR}/package.json`)
-
-	let contents = fs.readFileSync(indexTemplatePath, 'utf8');
-	let pkg = JSON.parse(fs.readFileSync(pkgPath, 'utf8'));
-
-	let comptempl = lodashTemplate(contents)
-
-	let manifest = await getJsonFile(resolve('./manifest.json')) || pkg["plugma"]["manifest"];
-
-	let input = resolve("/", manifest?.ui) || "/src/ui.ts"
-
-	if (getJsonFile(resolve('./manifest.json'))) {
-		contents = comptempl({ name: "figma", input })
-	}
-
-	await fse.outputFile(newIndexPath, contents);
-}
-
-function getRandomNumber() {
-	const min = 3000;
-	const max = 6999;
-	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
