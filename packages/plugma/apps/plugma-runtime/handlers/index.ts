df5f9b0313bf80d08101e97ce446c4a011aa1164
Gavin McFarland <5551+gavinmcfarland@users.noreply.github.com>
2025-03-21 14:13:38 +0000

    removed unused file

--- packages/plugma/apps/plugma-runtime/handlers/index.ts
+++ packages/plugma/apps/plugma-runtime/handlers/index.ts
@@ -1,12 +1,11 @@
 //@index('./!(*.test).*', f => `export * from '${f.path}.js';`)
-export * from './delete-client-storage.js';
-export * from './delete-file-storage.js';
-export * from './delete-root-pluginData.js';
-export * from './get-on-run-messages.js';
-export * from './hide-toolbar.js';
-export * from './maximize-window.js';
-export * from './minimize-window.js';
-export * from './save-on-run-messages.js';
-export * from './save-window-settings.js';
-export * from './setup.js';
+export * from "./delete-client-storage.js";
+export * from "./delete-file-storage.js";
+export * from "./delete-root-pluginData.js";
+export * from "./get-on-run-messages.js";
+export * from "./hide-toolbar.js";
+export * from "./maximize-window.js";
+export * from "./minimize-window.js";
+export * from "./save-on-run-messages.js";
+export * from "./save-window-settings.js";
 //@endindex
