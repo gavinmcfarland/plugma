735e61e97be21db0b35032ca449ed82e768cc573
Gavin McFarland <5551+gavinmcfarland@users.noreply.github.com>
2025-04-09 14:39:21 +0100

    Remove obsolete end-to-end test files for rectangle creation and example tests to streamline the test suite.

--- packages/plugma/test/sandbox/e2e/create-rectangles.spec.ts
+++ packages/plugma/test/sandbox/e2e/create-rectangles.spec.ts
@@ -1,6 +1,16 @@
 import { test, expect } from '@playwright/test'
 
-test('test', async ({ page }) => {
+test('create 1 rectangle', async ({ page }) => {
+	await page.goto('http://localhost:4000/')
+	await page.getByRole('spinbutton', { name: 'X-position' }).click()
+	await page.getByRole('spinbutton', { name: 'X-position' }).press('ArrowDown')
+	await page.getByRole('spinbutton', { name: 'X-position' }).press('ArrowDown')
+	await page.getByRole('spinbutton', { name: 'X-position' }).press('ArrowDown')
+	await page.getByRole('spinbutton', { name: 'X-position' }).press('ArrowDown')
+	await page.getByRole('button', { name: 'Create Rectangles' }).click()
+})
+
+test('create 10 rectangles', async ({ page }) => {
 	await page.goto('http://localhost:4000/')
 	await page.getByRole('spinbutton', { name: 'X-position' }).click()
 	await page.getByRole('spinbutton', { name: 'X-position' }).press('ArrowUp')
