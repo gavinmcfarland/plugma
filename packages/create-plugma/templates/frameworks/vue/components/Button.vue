39643b4ce6658af6a68062dc52b76ed07c21361d
Gavin McFarland <5551+gavinmcfarland@users.noreply.github.com>
2025-06-28 12:40:29 +0100

    Refine CONTRIBUTING.md for clarity and consistency in template creation
    - Updated section titles for improved readability.
    - Clarified instructions for creating directories and adding template files, ensuring a more straightforward process for users.

---
+++ packages/create-plugma/templates/frameworks/vue/components/Button.vue
@@ -0,0 +1,49 @@
+<script>
+export default {
+	name: 'Button',
+	props: {
+		href: {
+			default: null,
+		},
+		target: {
+			default: null,
+		},
+		style: {
+			default: () => ({}),
+		},
+		onClick: {
+			default: null,
+		},
+	},
+	methods: {
+		handleClick(event) {
+			if (this.onClick) {
+				this.onClick(event);
+			}
+		},
+	},
+};
+</script>
+
+<template>
+	<div>
+		<a v-if="href" :class="'Button'" :href="href" :target="target" :style="style" @click="handleClick">
+			<slot />
+		</a>
+		<button v-else :class="'Button'" :style="style" @click="handleClick">
+			<slot />
+		</button>
+	</div>
+</template>
+
+<style scoped>
+.Button {
+	display: block;
+	border-radius: 5px;
+	border: 1px solid var(--figma-color-border);
+	padding: 0 7px;
+	line-height: 22px;
+	text-decoration: none;
+	color: var(--figma-color-text);
+}
+</style>
